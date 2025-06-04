a8cb26a5ae7dc35b108ad221019ca6dc2a301e41
Umbet Nariman <umbet.nariman@mail.ru>
2025-06-05 00:51:07 +0500

    Update DATABASE_URL to use public Railway endpoint

--- gptproject4-1/backend/database.py
+++ gptproject4-1/backend/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:NEBHQVEvrEfUXoCWxcrnLTlxDnUCpZqz@dp-production-f7cf.up.railway.app:5432/railway"
+DATABASE_URL = "postgresql://postgres:NEBHQVEvrEfUXoCWxcrnLTlxDnUCpZqz@shuttle.proxy.rlwy.net:26971/railway"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
