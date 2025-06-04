26a39422ff7c3c708e642d6459a4b1c3a2ae6c45
Umbet Nariman <umbet.nariman@mail.ru>
2025-06-04 23:32:12 +0500

    Update DATABASE_URL for Railway

--- gptproject4-1/backend/database.py
+++ gptproject4-1/backend/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:postgres@localhost/cyber_ai_db"
+DATABASE_URL = "postgresql://postgres:NEBHQVEvrEfUXoCWxcrnLTlxDnUCpZqz@postgres.railway.internal:5432/railway"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
