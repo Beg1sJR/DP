b3aaa096a8818138407433ba7a9a260455e25240
Umbet Nariman <umbet.nariman@mail.ru>
2025-06-10 23:59:11 +0500

    changed CORSM

--- gptproject4-1/backend/main.py
+++ gptproject4-1/backend/main.py
@@ -53,7 +53,7 @@ app.openapi = lambda: custom_openapi(app)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://dp-front-azure.vercel.app/"],
+    allow_origins=["https://dp-front-azure.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
